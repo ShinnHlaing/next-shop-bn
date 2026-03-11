ea2cc46a69adaef779542f3ab5e7e41f7e95bd68
ShinnHlaing <hlaingthazinphoo.hp@gmail.com>
2026-03-11 23:48:10 +0630

    add: prettier for sorting class

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -1,5 +1,9 @@
 import React from "react";
 
 export default function BlogList() {
-  return <div>Blog list page</div>;
+  return (
+    <div className="mx-auto max-w-3xl bg-fuchsia-800 font-bold">
+      Blog list page
+    </div>
+  );
 }
