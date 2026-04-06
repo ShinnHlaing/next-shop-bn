732e8344015b1afa1bb74f9a3024b7789cde2fc3
ShinnHlaing <hlaingthazinphoo.hp@gmail.com>
2026-04-06 23:59:12 +0630

    fix : use different types of useParams for client components

--- app/product/[productId]/page.tsx
+++ app/product/[productId]/page.tsx
@@ -1,8 +1,17 @@
-import React from "react";
+"use client";
+//import { use } from "react"; //use in client component
+import { useParams } from "next/navigation";
 
-//using pageprops
-async function ProductId(props: PageProps<"/product/[productId]">) {
-  const { productId } = await props.params;
+// function ProductId(props: PageProps<"/product/[productId]">) {
+//   //const { productId } = props.params;
+//   const { productId } = use(props.params);
+
+//   return <div>ProductId - {productId}</div>;
+// }
+
+function ProductId() {
+  //const { productId } = props.params;
+  const { productId } = useParams<{ productId: string }>();
 
   return <div>ProductId - {productId}</div>;
 }
